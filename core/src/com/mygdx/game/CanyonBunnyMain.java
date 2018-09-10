bb91764ef4d4de0a8a0442219c7b1f5d3b509a63
christian crouthamel <cc9407@cs.ship.edu>
2018-09-09 17:53:00 -0700

    p117_canyoneBunnyMain

--- core/src/com/mygdx/game/CanyonBunnyMain.java
+++ core/src/com/mygdx/game/CanyonBunnyMain.java
@@ -3,6 +3,9 @@ package com.packtpub.libgdx.canyonbunny;
 import com.badlogic.gdx.ApplicationListener;
 import com.packtpub.libgdx.canyonbunny.game.WorldController;
 import com.packtpub.libgdx.canyonbunny.game.WorldRenderer;
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 
 public class CanyonBunnyMain implements ApplicationListener {
 
@@ -14,8 +17,17 @@ public class CanyonBunnyMain implements ApplicationListener {
 	private WorldController worldController;
 	private WorldRenderer worldRenderer;
 			
-	@Override public void create () { }
-    @Override public void render () { }
+	@Override public void create () { 
+	
+		// Set Libgdx log level to DEBUG
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		
+		// Initialize controller and renderer
+		worldController = new WorldController();
+		worldRenderer = new WorldRenderer(worldController);
+	}
+    
+	@Override public void render () { }
 	@Override public void resize (int width, int height) { }
 	@Override public void pause () { }
 	@Override public void resume () { }
