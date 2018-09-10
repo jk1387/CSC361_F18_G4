588084f5d9d0430874171a981ebeacd2f877eba0
Christian Crouthamel <cc9407@cs.ship.edu>
2018-09-10 13:55:47 -0400

    Fixed package location from class lecture

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.CSC361_F18_G4;
+import com.packtpub.libgdx.canyonbunny.CanyonBunnyMain;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CSC361_F18_G4(), config);
+		new LwjglApplication(new CanyonBunnyMain(), config);
 	}
 }
