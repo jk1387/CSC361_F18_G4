351faec41ebb41e33465a8612b02c4adaa4ae982
Jacob Kole <jk1387@ship.edu>
2018-09-14 15:52:52 -0400

    Started Mountains and added game object package to be accessed and added to.

---
+++ core/src/com/packtpub/libgdx/canyonbunny/game/objects/Mountains.java
@@ -0,0 +1,16 @@
+package com.packtpub.libgdx.canyonbunny.game.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.packtpub.libgdx.canyonbunny.game.Assets;
+
+public class Mountains {
+	
+	// 
+	private TextureRegion regMountainLeft;
+	private TextureRegion regMountainRight;
+	
+	// Length of the mountains
+	private int length;
+}
