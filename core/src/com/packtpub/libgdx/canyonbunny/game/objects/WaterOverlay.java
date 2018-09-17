4cd22af9d98463055f303e52ade908a565ff061e
chris <cc9407@cs.ship.edu>
2018-09-16 18:45:52 -0700

    water overlay class

---
+++ core/src/com/packtpub/libgdx/canyonbunny/game/objects/WaterOverlay.java
@@ -0,0 +1,10 @@
+package com.packtpub.libgdx.canyonbunny.game.objects;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.packtpub.libgdx.canyonbunny.game.Assets;
+
+public class WaterOverlay {
+
+	
+}
