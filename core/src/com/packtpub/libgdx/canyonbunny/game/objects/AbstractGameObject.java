b9b0c29e0807cc7412d846ca5bb4c474012d2c42
chris <cc9407@cs.ship.edu>
2018-09-23 21:21:31 -0700

    chapter 6 p197

--- core/src/com/packtpub/libgdx/canyonbunny/game/objects/AbstractGameObject.java
+++ core/src/com/packtpub/libgdx/canyonbunny/game/objects/AbstractGameObject.java
@@ -3,6 +3,7 @@ package com.packtpub.libgdx.canyonbunny.game.objects;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.MathUtils;
 
 public abstract class AbstractGameObject {
 
@@ -40,4 +41,50 @@ public abstract class AbstractGameObject {
 	
 	//abstract render method
 	public abstract void render (SpriteBatch batch);
+	
+	//Chapter 6
+	protected void updateMotionX (float deltaTime) {
+		
+		if (velocity.x != 0) {
+		// Apply friction
+		if (velocity.x > 0) {
+		       velocity.x =
+		       Math.max(velocity.x - friction.x * deltaTime, 0);
+		} 
+		else {
+		      velocity.x =
+		      Math.min(velocity.x + friction.x * deltaTime, 0);
+		     }
+		}
+		
+		// Apply acceleration
+		velocity.x += acceleration.x * deltaTime;
+		// Make sure the object's velocity does not exceed the
+		// positive or negative terminal velocity
+		velocity.x = MathUtils.clamp(velocity.x,
+		-terminalVelocity.x, terminalVelocity.x);
+		}
+		
+	    //motion y function
+	    protected void updateMotionY (float deltaTime) {
+		
+	    if (velocity.y != 0) {
+		// Apply friction
+		if (velocity.y > 0) {
+		     velocity.y = Math.max(velocity.y - friction.y *
+		     deltaTime, 0);
+		} 
+		else {
+		        velocity.y = Math.min(velocity.y + friction.y *
+		        deltaTime, 0);
+		       }
+		}
+		
+		// Apply acceleration
+		velocity.y += acceleration.y * deltaTime;
+		// Make sure the object's velocity does not exceed the
+		// positive or negative terminal velocity
+		velocity.y = MathUtils.clamp(velocity.y, -
+				terminalVelocity.y, terminalVelocity.y);
+		}
 }
