7278ee09c7eb43a240bf7ed487fbba33dca36b0c
Christian <cc9407@cs.ship.edu>
2018-10-01 13:49:25 -0400

    p233_WorldController and add comments

--- core/src/com/packtpub/libgdx/canyonbunny/screens/AbstractGameScreen.java
+++ core/src/com/packtpub/libgdx/canyonbunny/screens/AbstractGameScreen.java
@@ -10,6 +10,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.packtpub.libgdx.canyonbunny.game.Assets;
 
+/*
+ * abstract class to create methods for the game screen to use 
+ */
 public abstract class AbstractGameScreen implements Screen {
 	protected Game game;
 
@@ -27,6 +30,11 @@ public abstract class AbstractGameScreen implements Screen {
 
 	public abstract void pause();
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.badlogic.gdx.Screen#resume()
+	 * resume and dispose methods for the asset manager
+	 */
 	public void resume() {
 		Assets.instance.init(new AssetManager());
 	}
