0c1215e2cf67b503fb411be5d5cc4c69479323a0
Christian <cc9407@cs.ship.edu>
2018-10-01 14:12:53 -0400

    p260_GameScreen add import

--- core/src/com/packtpub/libgdx/canyonbunny/screens/GameScreen.java
+++ core/src/com/packtpub/libgdx/canyonbunny/screens/GameScreen.java
@@ -3,7 +3,7 @@ package com.packtpub.libgdx.canyonbunny.screens;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-
+import com.packtpub.libgdx.canyonbunny.util.GamePreferences;
 import com.packtpub.libgdx.canyonbunny.game.WorldController;
 import com.packtpub.libgdx.canyonbunny.game.WorldRenderer;
 
