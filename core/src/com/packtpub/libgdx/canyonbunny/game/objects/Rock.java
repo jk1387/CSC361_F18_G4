803e6d9da412e5c06cf6fc94491d85e8a8919bc7
Jacob Kole <jk1387@ship.edu>
2018-10-03 15:58:35 -0400

    Commented Rock and added in the controls and updates for how Rocks move up and down.

--- core/src/com/packtpub/libgdx/canyonbunny/game/objects/Rock.java
+++ core/src/com/packtpub/libgdx/canyonbunny/game/objects/Rock.java
@@ -1,68 +1,149 @@
 package com.packtpub.libgdx.canyonbunny.game.objects;
-/*
- * Author: Drake Conaway
- */
+
+// rock assets
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.packtpub.libgdx.canyonbunny.game.Assets;
+
+// used for rock movements
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Rocks are the platforms the BunnyHead stands and jumps on. These are the
+ * objects in the map the bunny cannot pass through, and hitting the sides and
+ * tops of these rocks have special collision results. They also move up and
+ * down at set intervals, starting at random times upon rendering.
+ * 
+ * @author Drake Conaway, Jacob Kole
+ */
 public class Rock extends AbstractGameObject {
-private TextureRegion regEdge;
-private TextureRegion regMiddle;
 
-private int length;
+	private TextureRegion regEdge;
+	private TextureRegion regMiddle;
 
-public Rock() {
-	init();
-}
-private void init() {
-	dimension.set(1,1.5f);
-	regEdge = Assets.instance.rock.edge;//set edge assets
-	regMiddle = Assets.instance.rock.middle; //set middle assets
-	//Start length of this rock
-	setLength(1);
-}
-public void setLength(int length) {
-	this.length = length; //set rock length to val
-	
-	// Update bounding box for collision detection
-	bounds.set(0, 0, dimension.x * length, dimension.y);
-}
-public void increaseLength(int amount) {
-	setLength(length + amount); //increase rock length
-	
-}
-@Override
-public void render(SpriteBatch batch) {
-	TextureRegion reg = null;
-	
-	float relX = 0;
-	float relY = 0;
-	
-	//Draw left edge
-	reg = regEdge;
-	relX -=dimension.x / 4;
-	batch.draw(reg.getTexture(), position.x + relX, position.y +
-			relY, origin.x, origin.y, dimension.x / 4, dimension.y,
-			scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(),
-			reg.getRegionWidth(), reg.getRegionHeight(), false, false);
-	
-	//Draw middle
-	relX = 0;
-	reg = regMiddle;
-	for (int i = 0; i < length; i++) {
-		batch.draw(reg.getTexture(), position.x + relX, position.y
-		+ relY, origin.x, origin.y, dimension.x, dimension.y,
-		scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(),
-		reg.getRegionWidth(), reg.getRegionHeight(), false, false);
-		relX += dimension.x;
+// variables used by Lerp to move the rocks up and down
+	private final float FLOAT_CYCLE_TIME = 2.0f; // how long it takes to go up and down
+	private final float FLOAT_AMPLITUDE = 0.25f; // how far the rock moves
+	private float floatCycleTimeLeft; // time left in the cycle
+	private boolean floatingDownwards; // is floating downward
+	private Vector2 floatTargetPosition; // next target position
+
+	private int length;
+
+	/**
+	 * Initialize the rock object.
+	 */
+	public Rock() {
+		init();
+	}
+
+	/**
+	 * Initialize the Rock middle pieces and edge pieces with
+	 * some set length. Also initializes the variables needed
+	 * to control the up and down movements of rocks. 
+	 */
+	private void init() {
+		dimension.set(1, 1.5f);
+
+		regEdge = Assets.instance.rock.edge;// set edge assets
+		regMiddle = Assets.instance.rock.middle; // set middle assets
+
+		// Start length of this rock
+		setLength(1);
+
+		// initialized to up direction
+		floatingDownwards = false;
+		// cycle time is randomly picked between 0 and half of CYCLE_TIME
+		floatCycleTimeLeft = MathUtils.random(0, FLOAT_CYCLE_TIME / 2);
+		// next target position
+		floatTargetPosition = null;
+	}
+
+	/**
+	 * Sets the length of the rock's boundaries.
+	 * @param length bounds of the rock
+	 */
+	public void setLength(int length) {
+		this.length = length; // set rock length to val
+
+		// Update bounding box for collision detection
+		bounds.set(0, 0, dimension.x * length, dimension.y);
+	}
+
+	/**
+	 * Increases the length of the rock's bounds
+	 * by some amount.
+	 * @param amount addition to length of rock bounds
+	 */
+	public void increaseLength(int amount) {
+		setLength(length + amount); // increase rock length
+
+	}
+
+	/**
+	 * Draws the Rock using assets for the rock edges and
+	 * the rock middles. These are knit together within
+	 * the overall bounds of the rock to form the full
+	 * rock as one single object.
+	 */
+	@Override
+	public void render(SpriteBatch batch) {
+		TextureRegion reg = null;
+
+		float relX = 0;
+		float relY = 0;
+
+		// Draw left edge
+		reg = regEdge;
+		relX -= dimension.x / 4;
+		batch.draw(reg.getTexture(), position.x + relX, position.y + relY, origin.x, origin.y, dimension.x / 4,
+				dimension.y, scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(), reg.getRegionWidth(),
+				reg.getRegionHeight(), false, false);
+
+		// Draw middle
+		relX = 0;
+		reg = regMiddle;
+		for (int i = 0; i < length; i++) {
+			batch.draw(reg.getTexture(), position.x + relX, position.y + relY, origin.x, origin.y, dimension.x,
+					dimension.y, scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(), reg.getRegionWidth(),
+					reg.getRegionHeight(), false, false);
+			relX += dimension.x;
 		}
-	
-	//Draw right edge
-	reg = regEdge;
-	batch.draw(reg.getTexture(),position.x + relX, position.y +
-			relY, origin.x + dimension.x / 8, origin.y, dimension.x / 4,
-			dimension.y, scale.x, scale.y, rotation, reg.getRegionX(),
-			reg.getRegionY(), reg.getRegionWidth(), reg.getRegionHeight(),
-			true, false);
-			}
+
+		// Draw right edge
+		reg = regEdge;
+		batch.draw(reg.getTexture(), position.x + relX, position.y + relY, origin.x + dimension.x / 8, origin.y,
+				dimension.x / 4, dimension.y, scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(),
+				reg.getRegionWidth(), reg.getRegionHeight(), true, false);
+	}
+
+	/**
+	 * Rocks update and need to move in the air by floating
+	 * up and down. The duration, distance moved, and start
+	 * times are controlled through here.
+	 */
+	@Override
+	public void update(float deltaTime) {
+		// update delta time
+		super.update(deltaTime);
+
+		// use delta time to calculate time left for that cycle
+		floatCycleTimeLeft -= deltaTime;
+		
+		// rocks need to know their original position
+		// upon initialization
+		if (floatTargetPosition == null)
+			floatTargetPosition = new Vector2(position);
+
+		// resets float time and flips direction
+		// uses Lerp
+		if (floatCycleTimeLeft <= 0) {
+			floatCycleTimeLeft = FLOAT_CYCLE_TIME;
+			floatingDownwards = !floatingDownwards;
+			floatTargetPosition.y += FLOAT_AMPLITUDE * (floatingDownwards ? -1 : 1);
+			position.lerp(floatTargetPosition, deltaTime);
+		}
+	}
+
 }
