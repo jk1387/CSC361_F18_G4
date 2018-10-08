6f5ae32a625e380e2a21d4607c21237b0f3b116f
Christian <cc9407@cs.ship.edu>
2018-10-08 15:43:41 -0400

    p339_AssetLevelDecoration

--- core/src/com/packtpub/libgdx/canyonbunny/game/Assets.java
+++ core/src/com/packtpub/libgdx/canyonbunny/game/Assets.java
@@ -129,6 +129,8 @@ public class Assets implements Disposable, AssetErrorListener {
 		public final AtlasRegion mountainLeft;
 		public final AtlasRegion mountainRight;
 		public final AtlasRegion waterOverlay;
+		public final AtlasRegion carrot;
+		public final AtlasRegion goal;
 
 		public AssetLevelDecoration(TextureAtlas atlas) {
 			cloud01 = atlas.findRegion("cloud01");
@@ -137,6 +139,8 @@ public class Assets implements Disposable, AssetErrorListener {
 			mountainLeft = atlas.findRegion("mountain_left");
 			mountainRight = atlas.findRegion("mountain_right");
 			waterOverlay = atlas.findRegion("water_overlay");
+			carrot = atlas.findRegion("carrot");
+			goal = atlas.findRegion("goal");
 		}
 	}
 
