cab7b5ffd06596ad18efeae9319d6d574c72d213
Christian <cc9407@cs.ship.edu>
2018-10-17 13:42:36 -0400

    add methods for menuscreen

--- core/src/com/packtpub/libgdx/canyonbunny/screens/MenuScreen.java
+++ core/src/com/packtpub/libgdx/canyonbunny/screens/MenuScreen.java
@@ -447,4 +447,40 @@ public class MenuScreen extends AbstractGameScreen {
 		btnMenuOptions.setVisible(false);
 		winOptions.setVisible(true);
 	}
+	
+	/*
+	 * method to show the menu buttons
+	 */
+	private void showMenuButtons (boolean visible) {
+		float moveDuration = 1.0f;
+		Interpolation moveEasing = Interpolation.swing;
+		float delayOptionsButton = 0.25f;
+		float moveX = 300 * (visible ? -1 : 1);
+		float moveY = 0 * (visible ? -1 : 1);
+		final Touchable touchEnabled = visible ? Touchable.enabled: Touchable.disabled;
+		
+		btnMenuPlay.addAction(moveBy(moveX, moveY, moveDuration, moveEasing));
+		btnMenuOptions.addAction(sequence(delay(delayOptionsButton),moveBy(moveX, moveY, moveDuration, moveEasing)));
+		SequenceAction seq = sequence();
+		
+		if (visible)
+		seq.addAction(delay(delayOptionsButton + moveDuration));
+		
+		seq.addAction(run(new Runnable() {
+		public void run () {
+		btnMenuPlay.setTouchable(touchEnabled);
+		btnMenuOptions.setTouchable(touchEnabled);
+		}}));
+		stage.addAction(seq);
+		}
+	
+	/*
+	 * method for showing the options window 
+	 */
+	private void showOptionsWindow (boolean visible,boolean animated) {
+			float alphaTo = visible ? 0.8f : 0.0f;
+			float duration = animated ? 1.0f : 0.0f;
+			Touchable touchEnabled = visible ? Touchable.enabled: Touchable.disabled;
+			winOptions.addAction(sequence(touchable(touchEnabled),alpha(alphaTo, duration)));
+		}
 }
